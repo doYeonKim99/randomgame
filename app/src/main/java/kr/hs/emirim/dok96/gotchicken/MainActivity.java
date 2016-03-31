9e1178a570c0c81a83f87210d52aaf6ce657cdf3
Doyeon Kim <ehdus859599@gmail.com>
2016-03-31 20:59:39 +0900

    #4 addbutton

--- app/src/main/java/kr/hs/emirim/dok96/gotchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dok96/gotchicken/MainActivity.java
@@ -2,12 +2,34 @@ package kr.hs.emirim.dok96.gotchicken;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    EditText mName;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText) findViewById(R.id.name);
+        mButton = (Button) findViewById(R.id.btn_showmethechicken);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mName.setText("");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"앙 진선띠 닭찌찌?",Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/kr/hs/emirim/dok96/gotchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/dok96/gotchicken/ResultActivity.java
@@ -2,14 +2,23 @@ package kr.hs.emirim.dok96.gotchicken;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by appcreator36 on 2016. 3. 28..
  */
 public class ResultActivity extends ActionBarActivity {
+    TextView mResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResult = (TextView) findViewById(R.id.result);
+        mResult.setText("결과");
     }
+}
